93deee377bbc5f046899a8c00166a8a7d63fcb7f
SrHugoGarcia <peke-vichugo900000@hotmail.com>
2023-04-23 22:44:08 -0600

    Subiendo Update

--- routes/clienteRoutes.js
+++ routes/clienteRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { createCliente } = require('../controllers/clienteController');
+const { createCliente,getAllClientes,getCliente,updateCliente } = require('../controllers/clienteController');
 const router = express.Router();
 
-router.route('/').post(createCliente)
+router.route('/').post(createCliente).get(getAllClientes)
+router.route('/:id').get(getCliente).path(updateCliente)
 module.exports = router;
